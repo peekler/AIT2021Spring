f2c6580d6fcfaf5071e00373486125a3930df618
Ekler Peter <>
2021-02-17 23:06:55 +0100

    TicTacToe final version: draw texts, bitmaps, reveal animation, etc.

--- TicTacToe/app/src/main/java/hu/ait/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/hu/ait/tictactoe/MainActivity.kt
@@ -3,6 +3,8 @@ package hu.ait.tictactoe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,10 +15,42 @@ class MainActivity : AppCompatActivity() {
 
         btnReset.setOnClickListener {
             ticView.resetGame()
+            revealTicTacToe()
         }
     }
 
     public fun showMessage(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
+
+    public fun setStatusText(status: String) {
+        tvStatus.text = status
+    }
+
+    fun revealTicTacToe() {
+        val x = ticView.getRight()
+        val y = ticView.getBottom()
+
+        val startRadius = 0
+        val endRadius = Math.hypot(ticView.getWidth().toDouble(),
+            ticView.getHeight().toDouble()).toInt()
+
+        val anim = ViewAnimationUtils.createCircularReveal(
+            ticView,
+            x,
+            y,
+            startRadius.toFloat(),
+            endRadius.toFloat()
+        )
+
+        anim.duration = 3000
+
+        ticView.setVisibility(View.VISIBLE)
+        anim.start()
+    }
+
+    public fun isFlagModeOn() : Boolean {
+        return buttonFlag.isChecked
+    }
+
 }
--- TicTacToe/app/src/main/java/hu/ait/tictactoe/ui/TicTacToeView.kt
+++ TicTacToe/app/src/main/java/hu/ait/tictactoe/ui/TicTacToeView.kt
@@ -1,21 +1,27 @@
 package hu.ait.tictactoe.ui
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.PointF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import hu.ait.tictactoe.MainActivity
+import hu.ait.tictactoe.R
 import hu.ait.tictactoe.model.TicTacToeModel
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private var paintBackGround: Paint
     private var paintLine: Paint
+    private var paintText: Paint
 
+    var bitmapBg = BitmapFactory.decodeResource(
+        context?.resources, R.drawable.grass
+    )
+
+    private var tmpPlayer: PointF? = null
 
 
     init {
@@ -27,6 +33,18 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
         paintLine.color = Color.WHITE
         paintLine.strokeWidth = 5f
         paintLine.style = Paint.Style.STROKE
+
+        paintText = Paint()
+        paintText.color = Color.GREEN
+        paintText.textSize = 60f
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        paintText.textSize = height / 3f
+
+        bitmapBg = Bitmap.createScaledBitmap(bitmapBg, width / 3, height / 3, false)
     }
 
 
@@ -35,10 +53,15 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
 
         canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintBackGround)
 
+        canvas?.drawBitmap(bitmapBg, 0f, 0f, null)
 
         drawGameArea(canvas)
 
         drawPlayers(canvas)
+
+        drawTmpPlayer(canvas)
+
+        canvas?.drawText("5", 0f, height / 3f, paintText)
 
     }
 
@@ -47,16 +70,24 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
         canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
 
         // two horizontal lines
-        canvas.drawLine(0f, (height / 3).toFloat(), width.toFloat(), (height / 3).toFloat(),
-                paintLine)
-        canvas.drawLine(0f, (2 * height / 3).toFloat(), width.toFloat(),
-                (2 * height / 3).toFloat(), paintLine)
+        canvas.drawLine(
+            0f, (height / 3).toFloat(), width.toFloat(), (height / 3).toFloat(),
+            paintLine
+        )
+        canvas.drawLine(
+            0f, (2 * height / 3).toFloat(), width.toFloat(),
+            (2 * height / 3).toFloat(), paintLine
+        )
 
         // two vertical lines
-        canvas.drawLine((width / 3).toFloat(), 0f, (width / 3).toFloat(), height.toFloat(),
-                paintLine)
-        canvas.drawLine((2 * width / 3).toFloat(), 0f, (2 * width / 3).toFloat(), height.toFloat(),
-                paintLine)
+        canvas.drawLine(
+            (width / 3).toFloat(), 0f, (width / 3).toFloat(), height.toFloat(),
+            paintLine
+        )
+        canvas.drawLine(
+            (2 * width / 3).toFloat(), 0f, (2 * width / 3).toFloat(), height.toFloat(),
+            paintLine
+        )
     }
 
     private fun drawPlayers(canvas: Canvas) {
@@ -69,13 +100,43 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
 
                     canvas.drawCircle(centerX, centerY, radius.toFloat(), paintLine)
                 } else if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CROSS) {
-                    canvas.drawLine((i * width / 3).toFloat(), (j * height / 3).toFloat(),
-                            ((i + 1) * width / 3).toFloat(),
-                            ((j + 1) * height / 3).toFloat(), paintLine)
+                    canvas.drawLine(
+                        (i * width / 3).toFloat(), (j * height / 3).toFloat(),
+                        ((i + 1) * width / 3).toFloat(),
+                        ((j + 1) * height / 3).toFloat(), paintLine
+                    )
 
-                    canvas.drawLine(((i + 1) * width / 3).toFloat(), (j * height / 3).toFloat(),
-                            (i * width / 3).toFloat(), ((j + 1) * height / 3).toFloat(), paintLine)
+                    canvas.drawLine(
+                        ((i + 1) * width / 3).toFloat(), (j * height / 3).toFloat(),
+                        (i * width / 3).toFloat(), ((j + 1) * height / 3).toFloat(), paintLine
+                    )
                 }
+            }
+        }
+    }
+
+    private fun drawTmpPlayer(canvas: Canvas) {
+        if (tmpPlayer != null) {
+            if (TicTacToeModel.nextPlayer
+                === TicTacToeModel.CIRCLE
+            ) {
+                canvas.drawCircle(
+                    tmpPlayer!!.x, tmpPlayer!!.y, (height / 6 - 2).toFloat(),
+                    paintLine
+                )
+            } else {
+                canvas.drawLine(
+                    tmpPlayer!!.x - width / 6,
+                    tmpPlayer!!.y - height / 6,
+                    tmpPlayer!!.x + width / 6,
+                    tmpPlayer!!.y + height / 6, paintLine
+                )
+                canvas.drawLine(
+                    tmpPlayer!!.x - width / 6,
+                    tmpPlayer!!.y + height / 6,
+                    tmpPlayer!!.x + width / 6,
+                    tmpPlayer!!.y - height / 6, paintLine
+                )
             }
         }
     }
@@ -85,7 +146,15 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
-        if (event?.action == MotionEvent.ACTION_DOWN) {
+        if (event?.action == MotionEvent.ACTION_MOVE) {
+            tmpPlayer = PointF(event.x, event.y)
+            invalidate()
+        }
+        else if (event?.action == MotionEvent.ACTION_UP) {
+
+
+            tmpPlayer = null
+
             val tX = event.x.toInt() / (width/3)
             val tY = event.y.toInt() / (height/3)
 
@@ -95,10 +164,15 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
                 TicTacToeModel.changeNextPlayer()
                 invalidate() // redraws the view, so eventually the onDraw will be called again
 
-                (context as MainActivity).showMessage("NEXT TURN")
+                (context as MainActivity).showMessage(context.getString(R.string.text_next_turn))
+
+                val next = if (TicTacToeModel.nextPlayer == TicTacToeModel.CIRCLE) "O" else "X"
+                var textStatus = context.getString(R.string.text_next_player, next)
+                (context as MainActivity).setStatusText(textStatus)
             }
 
         }
+
 
 
         return true
@@ -107,6 +181,14 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
     public fun resetGame(){
         TicTacToeModel.resetModel()
         invalidate()
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val w = View.MeasureSpec.getSize(widthMeasureSpec)
+        val h = View.MeasureSpec.getSize(heightMeasureSpec)
+        val d = if (w == 0) h else if (h == 0) w else if (w < h) w else h
+        setMeasuredDimension(d, d)
     }
 
 
